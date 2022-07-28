b936625d4893fcce76abae79787afed5788ebe43
Sunjoo IM <tjss1580@gmail.com>
2022-07-29 02:06:28 +0900

    [Bronze II] Title: 태보태보 총난타, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/17249. 태보태보 총난타/태보태보 총난타.py
+++ 백준/Bronze/17249. 태보태보 총난타/태보태보 총난타.py
@@ -1,8 +1,5 @@
-t = input()
-t_split = t.split('(^0^)')
+l, r = input().split('(^0^)')
 
-t_left = t_split[0]
-t_right = t_split[1]
-
-print(t_left.count('@'), t_right.count('@'))
+print(l.count('@'), r.count('@'))
+    
     
