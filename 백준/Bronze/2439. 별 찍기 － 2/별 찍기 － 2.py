e22c0b245c2d3e1f1705bdec7fc0051b1b54bbd5
Sunjoo IM <tjss1580@gmail.com>
2022-08-04 00:53:37 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,3 +1,8 @@
 N = int(input())
-for n in range(1, N + 1):
-    print(' ' * (N - n) + '*' * n)
+star = 0
+blank = N
+
+for n in range(N):
+    star += 1
+    blank -= 1
+    print(' ' * blank + '*' * star)
