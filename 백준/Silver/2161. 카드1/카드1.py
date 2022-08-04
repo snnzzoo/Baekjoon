11b806d370ebed911d3c471136fd11aced0b180e
Sunjoo IM <tjss1580@gmail.com>
2022-08-05 01:06:32 +0900

    [Silver V] Title: 카드1, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,11 +1,8 @@
-from collections import deque
+N = int(input())
+queue = list(range(1, N + 1))
 
-n = int(input())
-queue = deque(range(1, n + 1))
-    
 while len(queue) > 1:
-    print(queue.popleft(), end=' ')
-    queue.append(queue.popleft())
-        
-print(queue[0])    
+    print(queue.pop(0), end=' ')
+    queue.append(queue.pop(0))
     
+print(queue[0])
