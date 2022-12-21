a430a06c5838506f6088e154b723296ed3f23608
Sunjoo Im <tjss1580@gmail.com>
2022-12-22 01:46:59 +0900

    [Bronze V] Title: 윤년, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,6 @@
-year = int(input())
+n = int(input())
 
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print(1)
+if ((n % 4 == 0) and (n % 100 != 0)) or (n % 400 == 0):
+    print("1")
 else:
-    print(0)
+    print("0")
