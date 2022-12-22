9e4067aeb1c56ff08d54069e5b02f0c23bfe07b8
Sunjoo Im <tjss1580@gmail.com>
2022-12-22 23:30:40 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,11 +1,10 @@
-x = int(input())
-y = int(input())
-
-if x > 0 and y > 0:
-    print('1')
-elif x > 0 and y < 0:
-    print('4')
-elif x < 0 and y > 0:
-    print('2')
-else:
-    print('3')
+a = int(input())
+b = int(input())
+if a > 0 and b > 0:
+    print("1")
+elif a > 0 and b < 0:
+    print("4")
+elif a < 0 and b > 0:
+    print("2")
+elif a < 0 and b < 0:
+    print("3")
